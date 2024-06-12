3c4b2e2cb6467cbdd3dc99dceec7f24a9772a69e
70734 <bohicks96@gmail.com>
2024-06-12 16:32:58 -0500

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    returnString = '';
+    for(let i = string.length - 1; i>=0; i--){
+        returnString += string[i];
+    }
+    return returnString;
 
 };
 
