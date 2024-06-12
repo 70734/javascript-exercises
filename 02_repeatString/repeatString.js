0a19fc8b859b54dd8fd580abbbdd86d13c04b78e
70734 <bohicks96@gmail.com>
2024-06-12 16:22:21 -0500

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatCount) {
+    let repeatedString = ''
+    if(repeatCount >= 0){
+        for(let i = 0; i < repeatCount; i++){
+            repeatedString += string;
+        }
+    }
+    else{repeatedString = "ERROR";}
+    
+    return repeatedString;
 };
 
 // Do not edit below this line
