20803f81ee25b9cc412c3192c30a3c25f35dcf32
70734 <bohicks96@gmail.com>
2024-06-12 19:42:06 -0500

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
-};
+    let values = Array.from(arguments);
+    let array = values[0];
+    
+    delete values[0];
 
+    //loop through the remove values
+    for(i = 0; i < values.length; i++)
+        {
+            let index = array.indexOf(values[i]);
+            while(index != -1){ // exit if value not found
+                array.splice(index,1);
+                index = array.indexOf(values[i]); // find index of item
+                
+            }
+        }
+    return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
+
