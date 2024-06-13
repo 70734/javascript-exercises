486da4149de17cdd16432d0e71b05b796f5a36a6
70734 <bohicks96@gmail.com>
2024-06-12 20:04:18 -0500

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    if(!Number.isInteger(start) || !Number.isInteger(stop) || start < 0 || stop < 0){return "ERROR";}
+    if(start > stop){
+        let temp = stop;
+        stop = start;
+        start = temp;}
+    let sum = 0;
+    for(i = start; i <= stop; i++){
+        sum += i;
+    }
 
-};
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
