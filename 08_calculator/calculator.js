38e55db39f1c69952220cbaa54841a921bdffde2
70734 <bohicks96@gmail.com>
2024-06-19 17:52:06 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, item) => total + item, 0 );
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let array = [];
+  for(let i = num; i > 0; i--){array.push(i);}
+  return array.reduce((total, item) => total * item, 1);
 };
 
 // Do not edit below this line
