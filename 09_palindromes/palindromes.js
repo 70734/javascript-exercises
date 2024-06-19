a1fde04e94e599190d363fdf5d3d52c93e045822
70734 <bohicks96@gmail.com>
2024-06-19 18:36:06 -0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = [...string];
+    array = array.filter((char) => char.match(/[A-Za-z0-9]/));
+    let reverseArray = [...array];
+    reverseArray.reverse();
+    if(array.join().toLowerCase() === reverseArray.join().toLowerCase()){return true;}
+    else {return false;}
+    
 };
 
 // Do not edit below this line
