eed31b76fa9d364e45910ec304c2cd64cef21c71
70734 <bohicks96@gmail.com>
2024-06-19 19:46:14 -0500

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
+function getAge(death, birth){
+    if(!death){ death = new Date().getFullYear();}
+    return death - birth;
 };
 
+const findTheOldest = function(people) {
+    
+    people = people.sort((person1, person2) => 
+        {return(getAge(person2.yearOfDeath, person2.yearOfBirth) - getAge(person1.yearOfDeath,person1.yearOfBirth))})
+    return people[0];
+};
 // Do not edit below this line
 module.exports = findTheOldest;
