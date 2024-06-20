b50fa20c78fe8c7f620febe582f99b019bd41df8
70734 <bohicks96@gmail.com>
2024-06-19 19:15:38 -0500

    Complete getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    titleArray = []
+    array.forEach(element => { titleArray.push(element.title)});
+    return titleArray;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
